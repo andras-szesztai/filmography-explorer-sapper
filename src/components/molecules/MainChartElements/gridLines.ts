3f45495b1c5d6c99726957867f76e32c450c71ec
andras@newzoo.com <andras@newzoo.com>
2021-02-27 16:39:18 +0100

    fixed y axis number formatting

--- src/components/molecules/MainChartElements/gridLines.ts
+++ src/components/molecules/MainChartElements/gridLines.ts
@@ -3,6 +3,7 @@ import chroma from 'chroma-js'
 import type { ScaleLinear } from 'd3-scale'
 import { axisRight } from 'd3-axis'
 import { Transition } from 'd3-transition'
+import numeral from 'numeral'
 
 import {
   color,
@@ -17,7 +18,7 @@ interface IParams {
 }
 
 const enterUpdateExitGridLines = ({ yGridArea, yScale, width }: IParams) => {
-  select<SVGGElement, unknown>(yGridArea)
+  select<SVGGElement, number>(yGridArea)
     .call(axisRight(yScale).ticks(4).tickSize(width))
     .call((g) => {
       g.select('.domain').remove()
@@ -27,12 +28,13 @@ const enterUpdateExitGridLines = ({ yGridArea, yScale, width }: IParams) => {
         .transition()
         .duration(durationInMilliseconds.lg)
         .attr('opacity', 1)
-      g.selectAll('text')
+      g.selectAll<SVGGElement, number>('text')
         .attr('dx', -16)
         .attr('dy', -8)
         .attr('fill', chroma(color.light).alpha(opacity.midLow).hex())
         .attr('font-size', 14)
         .attr('opacity', 0)
+        .text((d) => `${numeral(d).format('0')}`)
         .transition()
         .duration(durationInMilliseconds.lg)
         .attr('opacity', 1)
